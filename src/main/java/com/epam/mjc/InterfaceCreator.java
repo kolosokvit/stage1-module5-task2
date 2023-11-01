b5c299e3fcf04d4a27fb0b65568ef8fa87d9bdd4
Vitali Kalaskou <kolosokvit@gmail.com>
2023-11-01 22:55:45 +0300

    Solved in functional way

--- src/main/java/com/epam/mjc/InterfaceCreator.java
+++ src/main/java/com/epam/mjc/InterfaceCreator.java
@@ -1,20 +1,10 @@
 package com.epam.mjc;
 
-import java.util.ArrayList;
-import java.util.List;
+import java.util.stream.Collectors;
 
 public class InterfaceCreator {
 
     public Operation<Integer> divideBy(Integer divider) {
-        return new Operation<Integer>() {
-            @Override
-            public List<Integer> apply(List<Integer> arg) {
-                List<Integer> result = new ArrayList<>();
-                for (Integer i : arg) {
-                    result.add(i / divider);
-                }
-                return result;
-            }
-        };
+        return arg -> arg.stream().map(i -> i / divider).collect(Collectors.toList());
     }
 }
